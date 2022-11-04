4720284b561d719048bb00f83f7b2feac2f172d7
Manuel Poell <manuelpoell@gmx.at>
2022-11-04 16:40:32 +0100

    added prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf'
+};
